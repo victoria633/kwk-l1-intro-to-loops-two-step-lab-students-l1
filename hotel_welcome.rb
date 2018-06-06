16a31da21eb7faf8ffb1db5d1572f2aa4ad71e68
victoria633 <victoria6411@hotmail.com>
2018-06-06 18:01:01 +0000

    automatically backed up by learn

---
+++ hotel_welcome.rb
@@ -0,0 +1,8 @@
+def tv_welcome(name,room_number)
+  
+  puts "welcome #{name.upcase}to the springhill suites"
+  puts "room #{room_number}"
+  
+end
+
+tv_welcome("Jonathan", "113")
---
+++ loops.rb
@@ -0,0 +1,33 @@
+# def dance
+#   moves_slay=0
+  
+# loop do
+# if moves_slay==10
+#   break
+# end
+# puts "step to the left"
+# puts "steps to the right"
+# puts "kick"
+# moves_slay=moves_slay+1
+
+# end
+#end
+#dance
+
+#DEFINE KODE WITH KLOSSY METHOD
+def kwk_camp
+  days = 2
+  days_left = 7
+  #MAKE A WHILE loop
+  while days < 10
+  puts "keep coming to Camp!!!"
+  #days = days + 1
+  puts "You have #{days_left} days left"
+  days += 1
+  days_left -=+ 1
+ end
+ puts "Camp is over:("
+end
+kwk_camp
+#A WHILE LOOP WORKS WHILE IT IS FALSE
+#AN UNTIL LOOPS WORKS UNTIL IT IS TRUE
